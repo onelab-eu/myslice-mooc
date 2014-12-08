2c9b9bab506d81d86855f99dc9f2f39c099eb207
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2014-12-08 18:23:39 +0100

    display

--- myops2/www/myops2.py
+++ myops2/www/myops2.py
@@ -24,7 +24,7 @@ def status():
         raise Exception("Unable to connect to the database")
     
     try:
-        cursor.execute("select distinct on (m.hostname) m.hostname,m.timestamp as last_checked,m.status from monitor m order by m.hostname, m.timestamp desc;")
+        cursor.execute("select distinct on (m.hostname) m.hostname,r.ipv4,r.ipv6,m.timestamp as last_checked,m.status from monitor m left join resources r on (r.hostname = m.hostname) order by m.hostname, m.timestamp desc;")
         resources = cursor.fetchall()
     except Exception as e:
         raise Exception("Unable to retrieve data from database: %s" % e)
