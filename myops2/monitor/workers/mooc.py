2270bc2e9b799f57ca8ba30557b1be6f3d588279
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-01-20 17:56:23 +0100

    MOOC: executes jobs waiting in the queue (on rethinkdb)

---
+++ myops2/monitor/workers/mooc.py
@@ -0,0 +1,91 @@
+'''
+    MyOps2 - a new monitoring system for PlanetLab and other testbeds
+
+    Workers to execute jobs remotely (MOOC)
+
+    (c) 2014 - 2015 Ciro Scognamiglio <ciro.scognamiglio@lip6.fr>
+'''
+
+import logging
+import time
+import json
+from datetime import datetime
+
+import rethinkdb as r
+from rethinkdb.errors import RqlRuntimeError, RqlDriverError
+
+from myops2.settings import Config
+import myops2.lib.remote as remote
+
+def process_job(num, input):
+    """
+    This worker will try to check for resource availability
+
+    """
+    logger = logging.getLogger(__name__)
+
+    logger.info("Agent %s starting" % num)
+
+    try :
+        c = r.connect(host=Config.rethinkdb["host"], port=Config.rethinkdb["port"], db=Config.rethinkdb['db'])
+    except r.RqlDriverError :
+        logger.error("Can't connect to RethinkDB")
+        raise SystemExit("Can't connect to RethinkDB")
+
+    while True:
+        job = input.get()
+        logger.info("Agent %s processing job %s" % (num, job))
+
+        j = r.table('jobs').get(job).run(c)
+
+        r.table('jobs').get(job).update({
+            'started': datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'),
+            'jobstatus': 'running',
+            'message': 'executing job'
+        }).run(c)
+
+        result = remote.setup(j['node'])
+        if not result['status'] :
+            logger.info("%s : Failed SSH access (%s)" % (j['node'], result['message']))
+            upd = {
+                'completed': datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'),
+                'jobstatus': 'finished',
+                'message': 'node not reachable',
+                'returnstatus': 1,
+                'stdout': '',
+                'stderr': result['message']
+            }
+        else :
+            logger.info("Running job on %s" % (j['node']))
+
+            if j['command'] == 'ping':
+                ret = json.loads(remote.script(j['node'], 'ping.py' + ' ' + j['parameters']['arg'] + ' ' + j['parameters']['dst']))
+            elif j['command'] == 'traceroute':
+                ret = json.loads(remote.script(j['node'], 'traceroute.py' + ' ' + j['parameters']['arg'] + ' ' + j['parameters']['dst']))
+            elif j['command'] == 'iperf':
+                ret = json.loads(remote.script(j['node'], 'iperf.py' + ' ' + j['parameters']['arg'] + ' ' + j['parameters']['dst']))
+            else :
+                ret = False
+
+            if ret:
+                upd = {
+                    'completed': datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'),
+                    'jobstatus': 'finished',
+                    'message': 'job completed',
+                    'returnstatus': ret['returnstatus'],
+                    'stdout': ret['stdout'],
+                    'stderr': ret['stderr']
+                }
+                logger.info("Command executed on %s" % (j['node']))
+            else:
+                upd = {
+                    'completed': datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'),
+                    'jobstatus': 'finished',
+                    'message': 'job error',
+                    'returnstatus': 1,
+                    'stdout': '',
+                    'stderr': 'unknown command'
+                }
+                logger.error("Command %s not found" % (j['command']))
+
+        r.table('jobs').get(job).update(upd).run(c)
