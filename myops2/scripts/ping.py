60862629b3b8b1f42146d5ada0cefc1935caa60d
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-01-20 17:57:16 +0100

    added remote script ping

---
+++ myops2/scripts/ping.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+#
+# Reads system information and returns them as a structure json string
+#
+# (c) 2015 Ciro Scognamiglio <ciro.scognamiglio@lip6.fr>
+#
+
+import json
+import sys
+import subprocess
+
+
+if __name__ == '__main__':
+
+    arguments = sys.argv
+    hostname = arguments[1:]
+
+    ping = subprocess.Popen(
+                ["ping", "-c", "5"] + arguments[1:],
+                stdout = subprocess.PIPE,
+                stderr = subprocess.PIPE
+                )
+    out, err = ping.communicate()
+
+    print json.dumps({
+        'returnstatus': ping.returncode,
+        'stdout': out,
+        'stderr': err
+    })
