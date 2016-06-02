f5942753b2f51d3c66e6d695db482b4f7f56d13a
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-06-02 17:03:38 +0200

    switched to iperf3

--- myops2/scripts/iperf.py
+++ myops2/scripts/iperf.py
@@ -52,20 +52,21 @@ def run_command(cmd):
     return proc.returncode, out, err
 
 if __name__ == '__main__':
+    iperf_executable = 'iperf3'
     # find the iperf package
-    is_iperf = run_command(['which', 'iperf'])
+    is_iperf = run_command(['which', iperf_executable])
 
     if not is_iperf[1]:
         
         if platform.architecture()[0] == '64bit':
-            commands = ['rpm', '-ivh', 'https://iperf.fr/download/fedora/iperf-2.0.8-2.fc23.x86_64.rpm']
+            commands = ['rpm', '-ivh', 'https://iperf.fr/download/fedora/iperf3-3.0.11-2.fc23.x86_64.rpm']
         else:
-            commands = ['rpm', '-ivh', 'https://iperf.fr/download/fedora/iperf-2.0.5-13.fc21.i686.rpm']
+            commands = ['rpm', '-ivh', 'https://iperf.fr/download/fedora/iperf3-3.0.11-2.fc23.i686.rpm']
         
         install_code, out, err = run_command(commands)
 
         if err: 
-            install_code, out, err = run_command(['yum', 'install', 'iperf'])
+            install_code, out, err = run_command(['yum', 'install', iperf_executable])
 
         if err:
             print json.dumps({
@@ -74,16 +75,16 @@ if __name__ == '__main__':
                 'stderr': err
             })
 
-    is_iperf = run_command(['which', 'iperf'])
+    is_iperf = run_command(['which', iperf_executable])
     
     agruments = sys.argv
     running_type = agruments[1]
     
     if running_type == '-s':
-        iperf = run_command_with_timeout(['iperf'] + agruments[1:])
+        iperf = run_command_with_timeout([iperf_executable] + agruments[1:])
     elif running_type == '-c':
         #print('runnning client')
-        iperf = run_command(['iperf'] + agruments[1:])
+        iperf = run_command([iperf_executable] + agruments[1:])
 
     print json.dumps({
         'returnstatus': iperf[0],
