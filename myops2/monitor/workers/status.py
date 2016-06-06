72de7882a0603cbbab4b4495a8e51af2ff92fe2d
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-06-06 11:32:47 +0200

    catch connection error

--- myops2/monitor/workers/status.py
+++ myops2/monitor/workers/status.py
@@ -22,49 +22,52 @@ def agent(num, input):
             Load node information from the testbed/facility DB (e.g. with SFA or local API)
             We get from there the node status
         '''
-        node = Query('Nodes').hostname(resource).execute().first()
-
-        if not node:
-            logger.info("Node disappeared : %s", (node.hostname))
-
-        if not node.enabled:
-            #logger.info("(%s) %s is not enabled" % (node.boot, node.hostname))
-            availability = 0
-            status = "disabled"
-
-        elif not node.is_running():
-            #logger.info(" (%s) %s is not running" % (node.boot, node.hostname))
-            availability = 0
-            status = "down"
+        try:
+            node = Query('Nodes').hostname(resource).execute().first()
+        except Exception as e:
+            logger.error("Query error : %s", (e))
         else:
-            # if not r:
-            #     print "+=> (%s) %s is not accessible" % (node.boot, node.hostname)
-            #     availability = 0
-            #     status = "no access"
-            # else :
-            #     print "+=> (%s) %s is ok" % (node.boot, node.hostname)
-            availability = 1
-            status = "up"
-                #updates info about the node (testing)
-                # d.info_resource(node.hostname, {
-                #     #'ipv4' : node.ip(4),
-                #     'ipv6' : node.ip(6),
-                # })
+            if not node:
+                logger.info("Node disappeared : %s", (node.hostname))
 
-        '''
-            Node access status: e.g. ssh, we try to do a setup on the node and report the result
-            We try also with nodes that are marked as disabled or not working anyway
-        '''
+            if not node.enabled:
+                #logger.info("(%s) %s is not enabled" % (node.boot, node.hostname))
+                availability = 0
+                status = "disabled"
 
-        result = r.setup(resource)
-        if not result['status'] :
-            logger.info("%s : Failed SSH access (%s)" % (resource, result['message']))
-        else :
-            logger.info("%s : Setup complete" % (resource))
+            elif not node.is_running():
+                #logger.info(" (%s) %s is not running" % (node.boot, node.hostname))
+                availability = 0
+                status = "down"
+            else:
+                # if not r:
+                #     print "+=> (%s) %s is not accessible" % (node.boot, node.hostname)
+                #     availability = 0
+                #     status = "no access"
+                # else :
+                #     print "+=> (%s) %s is ok" % (node.boot, node.hostname)
+                availability = 1
+                status = "up"
+                    #updates info about the node (testing)
+                    # d.info_resource(node.hostname, {
+                    #     #'ipv4' : node.ip(4),
+                    #     'ipv6' : node.ip(6),
+                    # })
 
-        s.resource({
-            "hostname": node.hostname,
-            "state": node.boot_state,
-            "access" : result
-        })
+            '''
+                Node access status: e.g. ssh, we try to do a setup on the node and report the result
+                We try also with nodes that are marked as disabled or not working anyway
+            '''
+
+            result = r.setup(resource)
+            if not result['status'] :
+                logger.info("%s : Failed SSH access (%s)" % (resource, result['message']))
+            else :
+                logger.info("%s : Setup complete" % (resource))
+
+            s.resource({
+                "hostname": node.hostname,
+                "state": node.boot_state,
+                "access" : result
+            })
 
